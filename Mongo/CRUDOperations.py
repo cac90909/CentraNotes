6f02d6e1d917e1f4ebecc1f6315ab2341a5fc226
Chris Castro <cacastro909@gmail.com>
2022-07-16 10:47:49 -0400

    Worked on CRUD Methods Utilizing MongoDB $set Operator

--- Mongo/CRUDOperations.py
+++ Mongo/CRUDOperations.py
@@ -1,6 +1,13 @@
 import Query
+import pymongo
 
 #Current idea for structuring of database items: (_id: <id>, data: <data>, metadata: <metadata>)
+
+#Options for keywords in conditionally targeting data
+#Each of these keywords is mapped to the equivalent PyMongo comparison operator
+#Ex: {"equals" : "$eq"}
+queryComparisonOptions = ["equals", "greaterThan", "greaterThanOrEquals", "insideOf", 
+                          "lessThan", "lessThanOrEquals", "notEquals", "notInsideOf"]
 
 #Insertions
 def insertItem(collection, id, dataItem, metadataItem):
@@ -12,15 +19,21 @@ def insertSubItem(collection): #Need to specify not only the item to be inserted
     pass
 
 #Selections
-def selectItem(collection):
-    baseQuery = Query.buildBaseQuery("_id", None, id)
+def selectItem(collection, id):
+    baseQuery = Query.buildQuery("_id", id)
     result = collection.find_one(baseQuery)
-def selectSubItem(id, path):
-    pass
+    return result
+def selectSubItem(collection, id, path):
+    #baseQuery = Query.buildQuery("_id", id)
+    baseQuery = {"_id":id}
+    baseProjection = {}
+    result = collection.find_one(baseQuery)
+    return result
 def selectSubItemConditionally(id, path, filter, filterValue):
     pass 
 
 #Deletions
+mongoUnsetOperator = "$unset"
 def deleteItem(collection, id):
     deletionObject = {"_id" : id}
     result = collection.delete_one(deletionObject)
@@ -34,13 +47,18 @@ def deleteSubItemConditionally(collection):
     pass
 
 #Updates
-def updateItem(collection):
-    pass
-def updateItemConditionally(collection, id, path, filter):
-    pass
+mongoSetOperator = "$set"
+def updateItem(collection, id, path, value):
+    filterObject = {"_id" : id}
+    updateObject = {mongoSetOperator : {path : value}}
+    collection.update_one(filterObject, updateObject)
+#TODO: come up with way to facilitate creation of filter objects
+def updateItemConditionally(collection, id, path, filter, value):
+    filterObject = {"_id" : id}
+    filterObject.update(filter) #This merges the two dict objects into one
+    updateObject= { mongoSetOperator : { path : value}}
+    collection.update(filterObject, updateObject, upsert=True, multi=True)
  
-
-
 
 
 
