187873b2dde28698643c495c511c212653645c16
Chris Castro <cacastro909@gmail.com>
2022-07-16 15:36:18 -0400

    Created Fucntions to allow for building of different types of queries, as well as some helper functions to add logic to queries, combine queries, add a field to a query.

--- Mongo/Query.py
+++ Mongo/Query.py
@@ -1,46 +1,75 @@
-#Queries
-mongoQueryComparisonOperators = {"equals": "$eq", #Matches values that are equal to a specified value.
-                    "greaterThan" : "$gt", #Matches values that are greater than a specified value.
-                    "greaterThanOrEquals" : "$gte", #Matches values that are greater than or equal to a specified value.
-                    "insideOf" : "$in", #Matches any of the values specified in an array.
-                    "lessThan" : "$lt", #Matches values that are less than a specified value.
-                    "lessThanOrEquals" : "$lte",#Matches values that are less than or equal to a specified value.
-                    "notEquals" : "$ne", #Matches all values that are not equal to a specified value.
-                    "notInsideOf" : "$nin", #Matches none of the values specified in an array.
-                    }
-#Builds base of queries
-#NOTE: operator can be None to allow for a basic field=value query
-def buildQuery(field, value):
-        return { field : value }
-def buildComparisonQuery(field, operator, value):
-    return { field : { mongoQueryComparisonOperators[operator] : value} }
+#Matches values that are equal to a specified value.
+def buildEqualsQuery(value):
+    return {"$eq" : value}
+#Matches all values that are not equal to a specified value.
+def buildNotEqualsQuery(value):
+    return {"$ne" : value}
+#Matches values that are greater than a specified value.
+def buildGreaterThanQuery(value):
+    return {"$gt" : value}
+#Matches values that are greater than or equal to a specified value.
+def buildGreaterThanOrEqualsQuery(value):
+    return {"$gte" : value}
+#Matches values that are less than a specified value.
+def buildLessThanQuery(value):
+    return {"$lt" : value}
+#Matches values that are less than or equal to a specified value.
+def buildLessThanOrEqualsQuery(value):
+    return {"$lte" : value}
+#when applying the exists operator, it will only return a document/sub-document if the specified field exists (it can have ANY value, just has to exist)
+def buildExistsQuery():
+    return {"$exists" : True}
+#when applying the exists operator, it will only return a document/sub-document if the specified field exists (it can have ANY value, just has to exist)
+def buildNotExistsQuery():
+    return {"$exists" : False}
+#Matches any of the values specified in an array.
+def buildInsideOfQuery(valuesList):
+    return {"$in" : valuesList}
+#Matches none of the values specified in an array.
+def buildNotInsideOfQuery(valuesList):
+    return {"$nin" : valuesList}
+#selects the documents where the value of a field is an array that contains all the specified elements.
+def buildMatchAllQuery(valuesList):
+    return {"$all" : valuesList}
+
+#performs a logical NOT operation on the specified <operator-expression> and selects the documents that do not match the <operator-expression>
+def addNotLogicToQuery(query):
+    return {"$not" : query}
+#he $elemMatch operator matches documents that contain an array field with at least one element that matches all the specified query criteria.
+def addElementMatchLogicToQuery(queryList):
+    return {"$elemMatch" : queryList}
+#queryList must be of type list
+#performs a logical AND operation on an array of one or more expressions
+def addAndLogicToQuery(queryList):
+    return {"$and" : queryList}
+#Logical OR operation on an array of two or more expressions and selects the documents that satisfy at least one of the expressions
+def addOrLogicToQuery(queryList):
+    return {"$or" : queryList}
+#performs a logical NOR operation on an array of one or more query expression and selects the documents that fail all the query expressions in the array.
+def addNorLogicToQuery(queryList):
+    return {"$nor" : queryList}
+#combines the query dictionary objects passed. Input should be of type list
+def combineQueries(queryList):
+    baseQuery = queryList[0]
+    for query in queryList[1:]:
+        baseQuery.update(query)
+    combinedQueries = baseQuery
+    return combinedQueries
+    
+#This function is going to probably be used in all query creations
+def addFieldToQuery(field, query):
+    return {field : query}
 
 
 
-mongoQueryOtherOperators = { "not" : "$not", #$not performs a logical NOT operation on the specified <operator-expression> and selects the documents that do not match the <operator-expression> 
-                    "exists" : "$exists" #when applying the exists operator, it will only return a document/sub-document if the specified field exists (it can have ANY value, just has to exist)
-}
 
-def buildNotQuery(field, value):
-    return { field : { mongoQueryOtherOperators["not"]  : value } } 
-def buildComparisonNotQuery(field, operator, value):
-    return { field : { mongoQueryOtherOperators["not"] : {mongoQueryComparisonOperators[operator] : value } } }
-def buildExistsQuery(field):
-    return { field : { mongoQueryOtherOperators["exists"] : True } }
-def buildNotExistsQuery(field):
-    return { field : { mongoQueryOtherOperators["exists"] : False } }
+
+
+
+
         
 
 
-mongoQueryLogicalOperators = { "or" : "$or", #Logical OR operation on an array of two or more expressions and selects the documents that satisfy at least one of the expressions
-                    "and" : "$and", #performs a logical AND operation on an array of one or more expressions
-                    "nor" : "$nor"
-
-}
-
-#Adds logical operator in front of two or more queries
-def addLogicalOperator(operator, queryList):
-    return { mongoQueryLogicalOperators[operator] : queryList }
 
 
 #TODO: make an array query builder
