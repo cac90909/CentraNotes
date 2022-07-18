35a45e9a9be45ef47d9a6c3dfcbdc835b4671c6b
Chris Castro <cacastro909@gmail.com>
2022-07-17 22:31:38 -0400

    Created 3 Query Building Templates in the QueryBuilder.py file

--- Mongo/Query/QueryBuilder.py
+++ Mongo/Query/QueryBuilder.py
@@ -1,18 +1,30 @@
 #Utilizes the Query.Helper python files to make it easier to construct Mongo DB Query Objects
+import Helper.QueryUtil
 
 
-def buildOneComparisonQuery(field, value, comparison):
-    pass
+#comparisonOperator options include (strings): =, !=, >, >=, <, <=, e (exists), !e (does not exist), in (inside of), !in, all (match all)
+def buildOneComparisonOneFieldQuery(field, comparisonOperator, value=None, valuesList=None):
+    baseQuery = Helper.QueryUtil.getBaseQueryFromOperator(comparisonOperator, value, valuesList)
+    completedQuery = Helper.QueryUtil.addFieldToQuery(field, baseQuery)
+    return completedQuery
+
+def buildTwoComparisonsOneFieldQuery(field, comparisonOperator1, comparisonOperator2, value1=None, valuesList1=None, value2=None, valuesList2=None):
+    baseQuery1 = Helper.QueryUtil.getBaseQueryFromOperator(comparisonOperator1, value1, valuesList1)
+    baseQuery2 = Helper.QueryUtil.getBaseQueryFromOperator(comparisonOperator2, value2, valuesList2)
+    queryList = [baseQuery1, baseQuery2]
+    combinedQuery = Helper.QueryUtil.combineQueries(queryList)
+    completedQuery = Helper.QueryUtil.addFieldToQuery(field, combinedQuery)
+    return completedQuery
+    
+#logicalOperator options include: and, or, nor
+def buildTwoComparisonsTwoFieldQueryWithLogic(field1, field2, comparisonOperator1, comparisonOperator2, logicalOperator, value1=None, valuesList1=None, value2=None, valuesList2=None):
+    baseQuery1 = Helper.QueryUtil.getBaseQueryFromOperator(comparisonOperator1, value1, valuesList1)
+    baseQuery2 = Helper.QueryUtil.getBaseQueryFromOperator(comparisonOperator2, value2, valuesList2)
+    baseQueryWithField1 = Helper.QueryUtil.addFieldToQuery(field1, baseQuery1)
+    baseQueryWithField2 = Helper.QueryUtil.addFieldToQuery(field2, baseQuery2)
+    queryList = [baseQueryWithField1, baseQueryWithField2]
+    completedQuery = Helper.QueryUtil.addLogicFromOperator(logicalOperator, queryList=queryList)
+    return completedQuery
 
 
-
-
-
-
-
-
-
-def buildQuery():
-
-   pass
 
