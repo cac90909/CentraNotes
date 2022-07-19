0c71e70f60c79b3a6cbc03937f6942be52bf9013
Chris Castro <cacastro909@gmail.com>
2022-07-19 18:51:12 -0400

    Resolved Dependency Issues that came from the previous commit (misreferencing paths/file names because they were changed)

--- Mongo/QueryBuilder.py
+++ Mongo/QueryBuilder.py
@@ -1,29 +1,29 @@
 #Utilizes the Query.Helper python files to make it easier to construct Mongo DB Query Objects
-import Helper.QueryUtil
+import QueryHelpers.QueryUtil
 
 
 #comparisonOperator options include (strings): =, !=, >, >=, <, <=, e (exists), !e (does not exist), in (inside of), !in, all (match all)
 def buildOneComparisonOneFieldQuery(field, comparisonOperator, value=None, valuesList=None):
-    baseQuery = Helper.QueryUtil.getBaseQueryFromOperator(comparisonOperator, value, valuesList)
-    completedQuery = Helper.QueryUtil.addFieldToQuery(field, baseQuery)
+    baseQuery = QueryHelpers.QueryUtil.getBaseQueryFromOperator(comparisonOperator, value, valuesList)
+    completedQuery = QueryHelpers.QueryUtil.addFieldToQuery(field, baseQuery)
     return completedQuery
 
 def buildTwoComparisonsOneFieldQuery(field, comparisonOperator1, comparisonOperator2, value1=None, valuesList1=None, value2=None, valuesList2=None):
-    baseQuery1 = Helper.QueryUtil.getBaseQueryFromOperator(comparisonOperator1, value1, valuesList1)
-    baseQuery2 = Helper.QueryUtil.getBaseQueryFromOperator(comparisonOperator2, value2, valuesList2)
+    baseQuery1 = QueryHelpers.QueryUtil.getBaseQueryFromOperator(comparisonOperator1, value1, valuesList1)
+    baseQuery2 = QueryHelpers.QueryUtil.getBaseQueryFromOperator(comparisonOperator2, value2, valuesList2)
     queryList = [baseQuery1, baseQuery2]
-    combinedQuery = Helper.QueryUtil.combineQueries(queryList)
-    completedQuery = Helper.QueryUtil.addFieldToQuery(field, combinedQuery)
+    combinedQuery = QueryHelpers.QueryUtil.combineQueries(queryList)
+    completedQuery = QueryHelpers.QueryUtil.addFieldToQuery(field, combinedQuery)
     return completedQuery
     
 #logicalOperator options include: and, or, nor
 def buildTwoComparisonsTwoFieldQueryWithLogic(field1, field2, comparisonOperator1, comparisonOperator2, logicalOperator, value1=None, valuesList1=None, value2=None, valuesList2=None):
-    baseQuery1 = Helper.QueryUtil.getBaseQueryFromOperator(comparisonOperator1, value1, valuesList1)
-    baseQuery2 = Helper.QueryUtil.getBaseQueryFromOperator(comparisonOperator2, value2, valuesList2)
-    baseQueryWithField1 = Helper.QueryUtil.addFieldToQuery(field1, baseQuery1)
-    baseQueryWithField2 = Helper.QueryUtil.addFieldToQuery(field2, baseQuery2)
+    baseQuery1 = QueryHelpers.QueryUtil.getBaseQueryFromOperator(comparisonOperator1, value1, valuesList1)
+    baseQuery2 = QueryHelpers.QueryUtil.getBaseQueryFromOperator(comparisonOperator2, value2, valuesList2)
+    baseQueryWithField1 = QueryHelpers.QueryUtil.addFieldToQuery(field1, baseQuery1)
+    baseQueryWithField2 = QueryHelpers.QueryUtil.addFieldToQuery(field2, baseQuery2)
     queryList = [baseQueryWithField1, baseQueryWithField2]
-    completedQuery = Helper.QueryUtil.addLogicFromOperator(logicalOperator, queryList=queryList)
+    completedQuery = QueryHelpers.QueryUtil.addLogicFromOperator(logicalOperator, queryList=queryList)
     return completedQuery
 
 
--- Mongo/QueryHelpers/QueryUtil.py
+++ Mongo/QueryHelpers/QueryUtil.py
@@ -1,8 +1,8 @@
 #Utility functions that assist in the building of queries. The following options are available: combining queries, adding a field to a query, getting a function from a passed operator
 #NOTE: this file serves as barrier between QueryBuilder and Base.Query/Base.ArrayQuery
-import Base.Query
-import Base.ArrayQuery
-import Logic.QueryLogic
+import QueryComponents.ValueQuery
+import QueryComponents.ListQuery
+import QueryComponents.QueryLogic
 
 #combines the query dictionary objects passed. Input should be of type list
 def combineQueries(queryList):
@@ -19,39 +19,39 @@ def addFieldToQuery(field, query):
 #Given an explict passed operator, will get the corresponding function that utilizes the mongo db equivalent operator
 def getBaseQueryFromOperator(operator, value=None, valuesList=None):
     if operator == "=":
-        return Base.Query.buildEqualsQuery(value)
+        return QueryComponents.ValueQuery.buildEqualsQuery(value)
     if operator == "!=":
-        return Base.Query.buildNotEqualsQuery(value)
+        return QueryComponents.ValueQuery.buildNotEqualsQuery(value)
     if operator == ">":
-        return Base.Query.buildGreaterThanQuery(value)
+        return QueryComponents.ValueQuery.buildGreaterThanQuery(value)
     if operator == ">=":
-        return Base.Query.buildGreaterThanOrEqualsQuery(value)
+        return QueryComponents.ValueQuery.buildGreaterThanOrEqualsQuery(value)
     if operator == "<":
-        return Base.Query.buildLessThanQuery(value)
+        return QueryComponents.ValueQuery.buildLessThanQuery(value)
     if operator == "<=":
-        return Base.Query.buildLessThanOrEqualsQuery(value)
+        return QueryComponents.ValueQuery.buildLessThanOrEqualsQuery(value)
     if operator == "e":
-        return Base.Query.buildExistsQuery()
+        return QueryComponents.ValueQuery.buildExistsQuery()
     if operator == "!e":
-        return Base.Query.buildNotExistsQuery()
+        return QueryComponents.ValueQuery.buildNotExistsQuery()
     if operator == "in":
-        return Base.ArrayQuery.buildInsideOfQuery(valuesList)
+        return QueryComponents.ListQuery.buildInsideOfQuery(valuesList)
     if operator == "!in":
-        return Base.ArrayQuery.buildNotInsideOfQuery(valuesList)
+        return QueryComponents.ListQuery.buildNotInsideOfQuery(valuesList)
     if operator == "all":
-        return Base.ArrayQuery.buildMatchAllQuery(valuesList)
+        return QueryComponents.ListQuery.buildMatchAllQuery(valuesList)
 
 #Given an explicitly passed operator, will add the corresponding logic to the list of queries/single query
 def addLogicFromOperator(operator, query=None, queryList=None):
     if operator == "not":
-        return Logic.QueryLogic.addNotLogicToQuery(query)
+        return QueryComponents.QueryLogic.addNotLogicToQuery(query)
     #Not and element match might be out of place here because and or and nor will follow similar syntaxes
     if operator == "em":
-        return Logic.QueryLogic.addElementMatchLogicToQuery(queryList)
+        return QueryComponents.QueryLogic.addElementMatchLogicToQuery(queryList)
     if operator == "and":
-        return Logic.QueryLogic.addAndLogicToQuery(queryList)
+        return QueryComponents.QueryLogic.addAndLogicToQuery(queryList)
     if operator == "or":
-        return Logic.QueryLogic.addOrLogicToQuery(queryList)
+        return QueryComponents.QueryLogic.addOrLogicToQuery(queryList)
     if operator == "nor":
-        return Logic.QueryLogic.addNorLogicToQuery(queryList)
+        return QueryComponents.QueryLogic.addNorLogicToQuery(queryList)
     
