5733479751d290ade0d10167ccc2502b0c692641
Chris Castro <cacastro909@gmail.com>
2022-08-07 12:27:27 -0400

    Created a second mongo section that is focused on setting practical coding goals and just trying to make them work. Previously, I was exhaustively exploring the tech and trying to solve for problems not yet existing. This commit setting the business context framework for the CRUD operations.

---
+++ Mongo.2/CRUD/Updates.py
@@ -0,0 +1,8 @@
+def UpdateUserID(oldUserId, newUserId): pass
+
+def UpdateFieldName(userId, oldFieldName, newFieldName): pass
+
+def UpdateFieldWithNewValue(userId, field, value): pass
+
+#Use Case for adding a value to an existing array data structure
+def UpdateFieldWithAdditionalValue(userId, field, value): pass
